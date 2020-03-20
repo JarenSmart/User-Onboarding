7b4940c619eb5dbf28b6e9c93eb52eb2d873c4e8
jaren <jarensmart@yahoo.com>
2020-03-19 21:49:34 -0400

    added formik and axios

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+import Form from "./Form.js";
 
 function App() {
+  const [sendState, setSendState] = useState([]);
+  const [userList, setUserList] = useState([]);
+
+  const userHandler = createUser => {
+    setSendState([...sendState, createUser]);
+    axios
+      .post("https://reqres.in/api/users", { createUser })
+      .then(response => {
+        console.log("This is your response: ", response);
+        setUserList([response.data]);
+      })
+      .catch(error => {
+        console.log("We have an error: ", error);
+      });
+  };
+
+  console.log("userList", userList);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Profile</h1>
+        <Form submitUser={userHandler} />
+        {userList.map(user => {
+          return (
+            <div>
+              <ul>
+                <li>Name: {user.createUser.name}</li>
+                <li>Email: {user.createUser.email}</li>
+                <li>Password: {user.createUser.password}</li>
+                <li>{user.createUser.tos === true ? "yes" : ""}</li>
+              </ul>
+            </div>
+          );
+        })}
       </header>
     </div>
   );
